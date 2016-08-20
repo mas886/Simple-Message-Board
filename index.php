31fc3df447b102c0758f739d01b8eb0d88fe1ebd
mas886 <mas886@users.noreply.github.com>
2016-08-20 21:02:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (isset($_REQUEST['name']) && isset($_REQUEST['message']) && $_REQUEST['name']
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Plataforma del missatge</title>
+    <title>Simple Message Board</title>
 
     <!-- Bootstrap -->
     <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
